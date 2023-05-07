86bb1dbf26d7833d0251ea4524d51504ae1e1e0d
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-05-07 09:12:47 +0800

    Remarks

--- wp-content/themes/iqms/modules/posts-column/module.php
+++ wp-content/themes/iqms/modules/posts-column/module.php
@@ -4,7 +4,7 @@ add_action( 'manage_dcm_posts_custom_column' , 'set_custom_edit_dcm_column_colum
 function set_custom_edit_dcm_columns( $columns ) {
 	
 	$columns['application-status'] = 'Application Status';
-	$columns['dco-review-status'] = 'Review Status';
+	$columns['dco-review-status'] = 'DCO Review Status';
 	$columns['review-status'] = 'Review Status';
 	$columns['approve-status'] = 'Approve Status';
 
@@ -16,15 +16,15 @@ function set_custom_edit_dcm_column_column( $column, $post_id ) {
 	switch ( $column ) {
 		case 'dco-review-status' :
 			$display = '—';
-			$reviewed_by = get_post_meta( $post_id, '_user_reviewed', true );
-			if ( $reviewed_by ) {
+			$dco_reviewed_by = get_post_meta( $post_id, '_user_dco_reviewed', true );
+			if ( $dco_reviewed_by ) {
 
-				$user = get_user_by('ID', $reviewed_by);
+				$user = get_user_by('ID', $dco_reviewed_by);
 				$name = $user->data->display_name;
 				$role = ( ($user->roles[0] ? $user->roles[0] : '') );
 
-				$reviewed_status = get_field( 'dco_review_status', $post_id );
-				$display = ( $reviewed_status == 'yes' ? '<label class="table-label-success">Accepted by: ' : '<label class="table-label-danger">Denied by: ' ) . $name . ' ('.$role.')</label>';
+				$dco_reviewed_status = get_field( 'dco_review_status', $post_id );
+				$display = ( $dco_reviewed_status == 'yes' ? '<label class="table-label-success">Accepted by: ' : '<label class="table-label-danger">Denied by: ' ) . $name . ' ('.$role.')</label>';
 
 			}
 			echo $display;
