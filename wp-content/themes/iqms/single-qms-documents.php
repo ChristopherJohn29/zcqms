da8c4f68f3705739f58d8b1fdba06f970b7e3801
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-05-18 07:32:37 +0800

    Update

--- wp-content/themes/iqms/single-qms-documents.php
+++ wp-content/themes/iqms/single-qms-documents.php
@@ -52,7 +52,7 @@ $prepared_by_position = get_field('user_position', 'user_'.$prepared_by_user->ID
 
 $author_id = get_post_field ('post_author', $post_id);
 
-var_dump($prepared_by);
+var_dump($prepared_by_user);
 // $prepared_by_position = get_field('user_position', 'user_'.$author_id);
 
 $users = get_field( 'users' );
