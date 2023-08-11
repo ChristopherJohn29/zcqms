a9633c2c5668de6d0d53773a08988a00cb532dbe
christophergamo29 <christopherjohngamo@gmail.com>
2023-08-11 08:44:32 +0800

    Update

--- wp-content/themes/iqms/modules/ncar-ia/js/scripts.js
+++ wp-content/themes/iqms/modules/ncar-ia/js/scripts.js
@@ -813,7 +813,7 @@
 								'</td>'+
 								'<td><div class="form-group file-upload" data-id="'+_correction_ind+'">' +
 								'<label for="evidences"><button type="" class="btn btn-info btn-sm upload-btn-new" data-id="'+_correction_ind+'">Select files</button></label>' +
-								'<div class="hidden file-group evidences" id="improvement-action-'+_correction_ind+'"><input type="hidden" data-url="' + v.correction_attachment_url + '" value="' + v.correction_attachment_id + '" data-title="' + v.correction_attachment_title + '" class="evidences"></div>' +
+								'<div class="hidden file-group evidences" id="improvement-action-'+_correction_ind+'">'+e_input_attachment+'</div>' +
 								'<input type="text" readonly class="selected_files form-control" id="improvement-action-file-'+_correction_ind+'" value="'+e_html_attachment+'">' +
 								'</div></td>'+
 								'<td class="hidden"><input type="text" class="form-control input-sm correction_remarks" placeholder="remarks" value="'+v.correction_remarks+'"></td>'+
@@ -868,10 +868,6 @@
 							$html2 += '' + '<tr>' + '<td colspan="1">' +
 							 '<input type="text" disabled class="form-control  correction_text" value="' + v.correction_text + '"><button class="btn btn-primary view-button" data-content="' + v.correction_text + '">view</button></td>' + 
 							 '<td><input type="date" disabled class="form-control target_date" value="' + v.target_date + '"></td>' + '<td><input type="date" disabled class="form-control correction_date" value="' + v.correction_date + '"></td>' +
-							 '<td><div class="form-group file-upload">' +
-								'<div class="hidden file-group evidences">'+e_input_attachment+'</div>' +
-								'<input type="text" readonly class="selected_files form-control" id="improvement-action-file" value="'+e_html_attachment+'">' +
-								'</div></td>'+
 							 '<td>' + '<input type="radio" name="correction_implemented_' + _correction_ind + '" class="correction_implemented" value="Yes" ' + (v.correction_implemented == 'Yes' ? 'checked' : '') + '> Yes' + '<input type="radio" name="correction_implemented_' + _correction_ind + '" class="correction_implemented" value="No" ' + (v.correction_implemented == 'No' ? 'checked' : '') + '> No' + '</td>' + '<td colspan="2"><input type="text" class="form-control input-sm correction_remarks" placeholder="remarks" value="' + v.correction_remarks + '"><button class="btn btn-primary view-button" data-content="' + v.correction_remarks + '">view</button></td>' + '<td><button class="close delete-correction"><span aria-hidden="true">×</span></button></td>' + '</tr>';
 							_correction_ind++;
 							
