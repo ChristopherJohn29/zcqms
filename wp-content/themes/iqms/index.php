c9b5896d7169f0241689bfdd451f72e2dd80af89
christophergamo29 <christopherjohngamo@gmail.com>
2023-09-05 07:45:16 +0800

    Update

--- wp-content/themes/iqms/index.php
+++ wp-content/themes/iqms/index.php
@@ -277,6 +277,9 @@ get_header(); ?>
                             <iframe src="https://calendar.google.com/calendar/embed?src=rennzzzublasquillo%40gmail.com&ctz=Asia%2FManila" style="border: 0" width="400" height="200" frameborder="0" scrolling="no"></iframe>
                         </div>
 
+                        <h1 class="global-title" >
+                            <strong>Contact us</strong>
+                        </h1>
                         <div class="calendar-wrapper">
                            <?=do_shortcode('[contact-form-7 id="eadc0bf" title="request file"]')?>
                         </div>
