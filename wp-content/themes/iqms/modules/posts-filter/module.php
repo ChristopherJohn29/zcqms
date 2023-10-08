b02aac5b8efc570b1a179014c133512d23bce49c
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-10-08 13:40:43 +0800

    Update

--- wp-content/themes/iqms/modules/posts-filter/module.php
+++ wp-content/themes/iqms/modules/posts-filter/module.php
@@ -59,11 +59,10 @@ function filter_post_fields() {
 		$approve_by_data = ( is_array( $approve_by_data ) ? $approve_by_data : [] );
 
 		var_dump($approve_by_data);
-		
+		var_dump($user_id);
+
 		if ( $user_id == $p['ID'] ) {
-
 			$approve_by = true;
-
 		}
 
 		if ( $approve_by === false ) {
