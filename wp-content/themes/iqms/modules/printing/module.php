1dfcb02f3dc7e5eb34406ccfe6a0d8564fa72c90
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-10-11 04:50:12 +0800

    UpdatE

--- wp-content/themes/iqms/modules/printing/module.php
+++ wp-content/themes/iqms/modules/printing/module.php
@@ -1,23 +1,18 @@
 <?php 
 
-function redirect_on_post_update($post_id) {
-    // Check if this is a post update (not a new post)
-    if (wp_is_post_revision($post_id) || defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-        return;
-    }
+function redirect_on_post_update($post_ID, $post, $update) {
+    // Check if the post is being updated and not a new post
+    if ($update) {
+        // Check if it's of a specific post type (replace 'your_post_type' with your actual post type)
+        if ($post->post_type === 'printing') {
+            // Define the URL you want to redirect to
+            $redirect_url = home_url().'/wp-admin/edit.php?post_type=printing'; // Change to your desired destination URL
 
-    // Get the post object
-    $post = get_post($post_id);
-
-    // Check if it's of a specific post type (replace 'your_post_type' with your actual post type)
-    if ($post->post_type === 'printing') {
-        // Define the URL you want to redirect to
-        $redirect_url = home_url().'/wp-admin/edit.php?post_type=printing'; // Change to your desired destination URL
-
-        // Perform the redirection
-        // wp_redirect($redirect_url);
-        exit;
+            // Perform the redirection
+            wp_redirect($redirect_url);
+            exit;
+        }
     }
 }
 
-add_action('save_post', 'redirect_on_post_update');
+add_action('wp_insert_post', 'redirect_on_post_update', 10, 3);
