a6ca2ebaab9d4a8dc2e1ef4a51db7e8cd33c23f3
Christopher John Gamo <christopherjohngamo@gmail.com>
2024-10-25 23:05:57 +0800

    update

--- wp-content/themes/iqms/js/disable-drag.js
+++ wp-content/themes/iqms/js/disable-drag.js
@@ -1,4 +1,21 @@
 jQuery(document).ready(function($) {
-    // Disable sortable functionality for meta boxes
-    $('.meta-box-sortables').sortable('disable');
+    // Function to remove sortable
+    function disableSortable() {
+        $('.meta-box-sortables').sortable('destroy');
+    }
+
+    // Disable sortable on page load
+    disableSortable();
+
+    // Use MutationObserver to detect changes in the DOM
+    const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if ($(mutation.target).hasClass('meta-box-sortables')) {
+                disableSortable();  // Disable sortable again if detected
+            }
+        });
+    });
+
+    // Observe changes in the DOM
+    observer.observe(document.body, { childList: true, subtree: true });
 });
