eb1cfaa8885c6ae8ba2ed58593691165d3710e3e
Christopher John Gamo <christopherjohngamo@gmail.com>
2024-12-02 20:19:36 +0800

    updatE

--- wp-content/themes/iqms/modules/admin-enqueue/module.php
+++
@@ -1,6 +0,0 @@
-<?php
-function load_custom_wp_admin_style(){
-    wp_enqueue_style( 'custom-css', get_stylesheet_directory_uri() . '/modules/admin-enqueue/css/styles.css' );
-    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/modules/admin-enqueue/js/scripts.js' );
-}
-add_action('admin_enqueue_scripts', 'load_custom_wp_admin_style');
